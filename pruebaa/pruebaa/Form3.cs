1865900f2160859d06eb0de56e5ba2e877573b91
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2022-09-13 15:08:08 -0300

    g

--- pruebaa/pruebaa/Form3.cs
+++ pruebaa/pruebaa/Form3.cs
@@ -12,11 +12,11 @@ namespace pruebaa
 {
     public partial class Form3 : Form
     {
-  
-        PictureBox[] pictureBoxesArray = new PictureBox[4];
+ //creo array para guardar las fotos de abajo, array con las 3 fotos y un random que despues uso para asignar las fotos a los pBox
+        PictureBox[] pictureBoxesArray = new PictureBox[3];
         Image[] posiblesEmociones = new Image[3];
         Random numEmocion = new Random();
-
+        
 
         public Form3()
         {
@@ -26,30 +26,36 @@ namespace pruebaa
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            pictureBoxesArray[0] = pB1;
-            pictureBoxesArray[1] = pB2;
-            pictureBoxesArray[2] = pB3;
-            pictureBoxesArray[3] = pB4;
+ //agrego los pBox que cree por pantalla al array pictureBoxesArray
+           
+            pictureBoxesArray[0] = pB2;
+            pictureBoxesArray[1] = pB3;
+            pictureBoxesArray[2] = pB4;
 
-
+ //Esto es una funcion para que las imagenes se vean centradas en el pBox
             for (int i = 0; i < pictureBoxesArray.Length; i++)
             {  
                 pictureBoxesArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
             }
-       
+            pB1.SizeMode = PictureBoxSizeMode.StretchImage;
+ //Cargo la imagen de la compu y las meto en el array      
             posiblesEmociones[0] = pruebaa.Properties.Resources.pictocont;
             posiblesEmociones[1] = pruebaa.Properties.Resources.pictoenojo;
             posiblesEmociones[2] = pruebaa.Properties.Resources.pictotriste;
-            pictureBoxesArray[0].Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)];
-            pictureBoxesArray[1].Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)];
-            pictureBoxesArray[2].Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)];
-            pictureBoxesArray[3].Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)]; 
+            //Asigno una emocion a cada pBox aleatoriamente
+
+            pB1.Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)];
+
+            for(int i = 0; i<pictureBoxesArray.Length; i++){
+                pictureBoxesArray[i].Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)];
+            }
+         
         }
 
 
         private void btnstart_Click(object sender, EventArgs e)
         {
-            pictureBoxesArray[0].Show();
+            
         }
     }
 }
