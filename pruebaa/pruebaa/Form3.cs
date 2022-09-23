453da80f10908516890eecadbfe5a07d6379c4d3
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2022-09-23 16:10:44 -0300

    rANDOM SIN REPETIR

--- pruebaa/pruebaa/Form3.cs
+++ pruebaa/pruebaa/Form3.cs
@@ -14,9 +14,9 @@ namespace pruebaa
     {
  //creo array para guardar las fotos de abajo, array con las 3 fotos y un random que despues uso para asignar las fotos a los pBox
         PictureBox[] pictureBoxesArray = new PictureBox[3];
-        Image[] posiblesEmociones = new Image[3];
-        
-        Random numEmocion = new Random();
+        List <Image> posiblesEmociones = new List<Image>();
+       List <int> numEmocion = new List<int> ();
+        Random raux = new Random();
         
 
         public Form3()
@@ -27,37 +27,47 @@ namespace pruebaa
 
         private void Form3_Load(object sender, EventArgs e)
         {
- //agrego los pBox que cree por pantalla al array pictureBoxesArray
-           
+            //agrego los pBox que cree por pantalla al array pictureBoxesArray
+
             pictureBoxesArray[0] = pB2;
             pictureBoxesArray[1] = pB3;
             pictureBoxesArray[2] = pB4;
 
- //Esto es una funcion para que las imagenes se vean centradas en el pBox
-            for (int i = 0; i < pictureBoxesArray.Length; i++)
-            {  
-                pictureBoxesArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
+            //Esto es una funcion para que las imagenes se vean centradas en el pBox
+            for (int x = 0; x < pictureBoxesArray.Length; x++)
+            {
+                pictureBoxesArray[x].SizeMode = PictureBoxSizeMode.StretchImage;
             }
             pB1.SizeMode = PictureBoxSizeMode.StretchImage;
-            
- //Cargo la imagen de la compu y las meto en el array      
-            posiblesEmociones[0] = pruebaa.Properties.Resources.pictocont;
-            posiblesEmociones[1] = pruebaa.Properties.Resources.pictoenojo;
-            posiblesEmociones[2] = pruebaa.Properties.Resources.pictotriste;
 
-            
+            //Cargo la imagen de la compu y las meto en el array      
+            posiblesEmociones.Add(pruebaa.Properties.Resources.pictocont);
+            posiblesEmociones.Add(  pruebaa.Properties.Resources.pictoenojo);
+            posiblesEmociones.Add( pruebaa.Properties.Resources.pictotriste);
+
+
+
             //Asigno una emocion a cada pBox aleatoriamente
+            Console.WriteLine(posiblesEmociones.Count);
+            // for creo contador para pb 
+            int cont = 0;
+            while (posiblesEmociones.Count > 0)
+            {
 
-            pB1.Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)];
-       
 
-            for(int i = 0; i<pictureBoxesArray.Length; i++){
-                pictureBoxesArray[i].Image = posiblesEmociones[(numEmocion.Next(0, 2) + 1)];
+                int i = raux.Next(0, posiblesEmociones.Count);
+
+                pictureBoxesArray[cont].Image = posiblesEmociones[i];
+
+                posiblesEmociones.RemoveAt(i);
+                cont++;
             }
+            Console.WriteLine(posiblesEmociones.Count);
+
+
+
          
-        }
-
-
+}
         private void btnstart_Click(object sender, EventArgs e)
         {
             
