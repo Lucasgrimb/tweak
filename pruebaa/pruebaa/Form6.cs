9e56b0acb7b725dc6bf6e37b7d4916160afd6060
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2022-10-04 09:10:58 -0300

    empiezo a armar logica de asignacion

--- pruebaa/pruebaa/Form6.cs
+++ pruebaa/pruebaa/Form6.cs
@@ -14,7 +14,9 @@ namespace pruebaa
     {
         PictureBox[] pBpictos = new PictureBox[3];
         PictureBox[] pBdibujos = new PictureBox[3];
-        List<Image> posiblesEmociones = new List<Image>();
+        int[] nPicto = new int[3];
+        List<Image> pictos = new List<Image>();
+        List<Image> dibujos = new List<Image>();
         Random rand = new Random();
         public Form6()
         {
@@ -30,6 +32,52 @@ namespace pruebaa
             pBdibujos[0] = pB1d;
             pBdibujos[1] = pB2d;
             pBdibujos[2] = pB3d;
+
+            // pongo las imagenes en un formato especial para que se vean centradas
+            for (int x = 0; x < pBpictos.Length; x++)
+            {
+                pBpictos[x].SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            for (int x = 0; x < pBdibujos.Length; x++)
+            {
+                pBdibujos[x].SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            // Agrego las imagenes de resources a la lista con las emociones que se usarán en el nivel
+            pictos.Add(pruebaa.Properties.Resources.contento);
+            pictos.Add(pruebaa.Properties.Resources.triste);
+            pictos.Add(pruebaa.Properties.Resources.enfadado);
+            pictos.Add(pruebaa.Properties.Resources.asustado);
+            pictos.Add(pruebaa.Properties.Resources.sorprendido);
+            pictos.Add(pruebaa.Properties.Resources.cansado);
+            pictos.Add(pruebaa.Properties.Resources.hambriento);
+            pictos.Add(pruebaa.Properties.Resources.sediento);
+            pictos.Add(pruebaa.Properties.Resources.enfermo);
+            pictos.Add(pruebaa.Properties.Resources.aburrido);
+            pictos.Add(pruebaa.Properties.Resources.confundir);
+            pictos.Add(pruebaa.Properties.Resources.preocupado);
+
+            dibujos.Add(pruebaa.Properties.Resources.contentoD);
+            dibujos.Add(pruebaa.Properties.Resources.tristeD);
+            dibujos.Add(pruebaa.Properties.Resources.enfadadoD);
+            dibujos.Add(pruebaa.Properties.Resources.asustadoD);
+            dibujos.Add(pruebaa.Properties.Resources.sorprendidoD);
+            dibujos.Add(pruebaa.Properties.Resources.cansadoD);
+            dibujos.Add(pruebaa.Properties.Resources.hambrientoD);
+            dibujos.Add(pruebaa.Properties.Resources.sedientoD);
+            dibujos.Add(pruebaa.Properties.Resources.enfermoD);
+            dibujos.Add(pruebaa.Properties.Resources.aburridoD);
+            dibujos.Add(pruebaa.Properties.Resources.preocupadoD);
+
+            //asigno emociones a pBpictos
+            for (int i = 0;i< 3; i++) {
+                int x = rand.Next(0, pictos.Count);
+                pBpictos[i].Image = pictos[x];
+                pictos.RemoveAt(x);
+                nPicto[i] = x;
+            }
+
+            
+
         }
     }
 }
